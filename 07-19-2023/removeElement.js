eddd670d91d6e3187e48810ad46a9f13c81b1df0
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-07-19 11:37:34 -0500

    add removeElement

---
+++ 07-19-2023/removeElement.js
@@ -0,0 +1,28 @@
+/*
+p: array of integers, and integer target
+r: return number of elements that aren't target val(integer)
+    must use approach that restablish arr with _
+ep:
+    use single pointer approach
+    have var starting at 0;
+    for loop to iterate over nums
+        if num !== val
+        nums[currentIndex] = nums[i] 
+        currentIndex++
+        
+    array.length = current index
+    return array
+*/
+
+var removeElement = function (nums, val) {
+  let currentIndex = 0;
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (num !== val) {
+      nums[currentIndex] = num;
+      currentIndex++;
+    }
+  }
+  nums.length = currentIndex;
+  return nums.length;
+};
